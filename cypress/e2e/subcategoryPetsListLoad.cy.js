26bb5ac1f02857d854af5db18da04f5bdc2e5562
Srbislav98 <srbislav30111998@gmail.com>
2023-05-25 16:04:27 +0200

    fix: subcategoryPetsListLoad test

--- cypress/e2e/subcategoryPetsListLoad.cy.js
+++ cypress/e2e/subcategoryPetsListLoad.cy.js
@@ -1,6 +1,6 @@
 import SubcategoryPetPage from "../page-objects/subcategoryPets.page";
 
-describe("Pet short description", () => {
+describe("Subcategory List Of Pets Load", () => {
   const subcategoryPage = new SubcategoryPetPage();
 
   beforeEach(() => {
@@ -8,11 +8,28 @@ describe("Pet short description", () => {
     cy.contains("Enter the Store").click();
   });
 
-  it('should be able to load short description of chosen pet', () => {
+  it('should be able to load list of pets in subcategory by searching', () => {
     cy.fixture('pet.json').then((petData) => {
       cy.get('input[name="keyword"]').type('Iguana');
       cy.contains('Search').click();
       cy.contains(petData.description).click();
+  
+      subcategoryPage.getItemRows().then((itemRows) => {
+        itemRows.each((index, itemRow) => {
+          subcategoryPage.getItemDetails(cy.wrap(itemRow)).then((itemDetails) => {
+            expect(itemDetails.itemId).to.equal(petData.itemId);
+            expect(itemDetails.productId).to.equal(petData.productId);
+            expect(itemDetails.listPrice).to.equal(petData.price.toFixed(2));
+          });
+        });
+      });
+    });
+  });
+
+  xit('should be able to load list of pets in subcategory by chosing category and then subcategory', () => {
+    cy.fixture('pet.json').then((petData) => {
+      cy.get('#SidebarContent a[href*="REPTILES"]').click();
+      cy.contains(petData.productId).click();
   
       subcategoryPage.getItemRows().then((itemRows) => {
         itemRows.each((index, itemRow) => {
